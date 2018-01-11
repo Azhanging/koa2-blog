fc177b25779e5a1df3ac24949379539fc0a516a7
Blue <azhanging@qq.com>
2018-01-11 20:29:46 +0800

    更新

--- router/index.js
+++ router/index.js
@@ -1,37 +1,39 @@
 const Router = require('koa-router');
 
-	const router = new Router();
+const router = new Router();
 
-	router.get('/', (ctx, next) => {
-		ctx.body = `<p>index</p>`;
+router.redirect('/aaa','/home');
+
+// router.get('/', (ctx, next) => {
+// 	ctx.body = `<p>index</p>`;
+// });
+
+router.get('/home', async (ctx, next) => {
+
+	ctx.cookies.set('laosiji', 'gaoshiqing');
+
+	await ctx.render('/index/index', {
+		list: [
+			{
+				title: 'title1',
+				content: 'content1'
+			}, {
+				title: 'title2',
+				content: 'content2'
+			}, {
+				title: 'title3',
+				content: 'content3'
+			}, {
+				title: 'title4',
+				content: 'content4'
+			}, {
+				title: 'title5',
+				content: 'content5'
+			}
+		],
+		a: '1',
+		b: 4
 	});
-
-	router.get('/home', async (ctx, next) => {
-
-		ctx.cookies.set('laosiji','gaoshiqing');
-
-		await ctx.render('/index/index',{
-			list:[
-				{
-					title:'title1',
-					content:'content1'
-				},{
-					title:'title2',
-					content:'content2'
-				},{
-					title:'title3',
-					content:'content3'
-				},{
-					title:'title4',
-					content:'content4'
-				},{
-					title:'title5',
-					content:'content5'
-				}
-			],
-			a:'1',
-			b:4
-		});
 
 }).post('/home', async (ctx, next) => {
 
